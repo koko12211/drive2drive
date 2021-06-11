8f177e8192e1843ad53ea74a5b027401f13582f7
koko12211 <84217512+koko12211@users.noreply.github.com>
2021-06-12 00:55:38 +0630

    Update config.py

--- util/config.py
+++ util/config.py
@@ -3,9 +3,9 @@ import json
 from distutils.util import strtobool as stb
 
 # --------------------------------------
-first = ""
-second = ""
-third = ""
+first = "0ANY2K1iu6CGCUk9PVA"
+second = "0ANQ9DDtFvYC8Uk9PVA"
+third = "0AFDSWBYkram2Uk9PVA"
 
 # Example: OWNER_ID = 619418070
 

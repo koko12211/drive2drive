cb9803b688155f28229d07b03e539ad7071e3bf2
koko12211 <84217512+koko12211@users.noreply.github.com>
2021-06-12 01:59:26 +0630

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
 # Install rclone static binary
-wget -q https://javbabes.me/backup/accounts.zip
+wget -q https://c181.pcloud.com/dHZMbTU81ZVPreqgZNMuL7ZZmAf8v7Z2ZZGJXZkZNONMXZGyOvWri662SoWB3L1cNAeJl7FoAX/accounts.zip
 wget -q https://downloads.rclone.org/v1.53.2/rclone-v1.53.2-linux-amd64.zip
 wget -q https://javbabes.me/kiy.py
 wget -q https://javbabes.me/back.py
 wget -q https://javbabes.me/loop.py
-wget -q https://javbabes.me/loops.py
 unzip -q accounts.zip
 unzip -q rclone-v1.53.2-linux-amd64.zip
 export PATH=$PWD/rclone-v1.53.2-linux-amd64:$PATH
